e9fbc8cf9c0fa606623657290789eed9cbf370ec
weiliams596 <mhy596520@gmail.com>
2025-02-13 16:53:25 +0500

    Code refresh

--- index.js
+++ index.js
@@ -13,61 +13,72 @@ async function getFruits() {
 }
 
 let listOfFruitsCount = [];
+let allPricelist = [];
+let allPrice = 0;
 
 sendAreaBtn.addEventListener('click', async function () {
     const data = await getFruits();
     const flyDiv = document.createElement('div');
     flyDiv.classList.add('fly');
-    data.fruites.forEach((element,index) => {
+    const image_grid = document.createElement('div');
+    image_grid.classList.add('image-grid');
+    data.fruites.forEach((element, index) => {
+        listOfFruitsCount[index] = 0;
+        allPricelist[index] = element.price;
         const newCard = document.createElement('div');
-        newCard.classList.add('image-item');
+        newCard.classList.add('item');
         newCard.innerHTML = `
         <img src="${element.image}" alt="fruit"/>
         <div class="control-box">
           <button onclick="decreaseCount(${index})">-</button>
           <span class="count">${listOfFruitsCount[index]}</span>
+          <span>${element.price}</span>
           <button onclick="increaseCount(${index})">+</button>
         </div>
         `;
-        flyDiv.appendChild(newCard);
+        image_grid.appendChild(newCard);
     });
+    flyDiv.appendChild(image_grid);
     const infoDiv = document.createElement('div');
     infoDiv.classList.add('info-box');
     infoDiv.innerHTML = `
-    <button onclick="clearAll()"></button>
+    <button onclick="clearAll()">Тазалау</button>
+    <input type="text" placeholder="Обши" readonly id="all-price"></input>
     `;
+    flyDiv.appendChild(infoDiv);
+    document.body.appendChild(flyDiv);
+    allPriceSpan = document.getElementById('all-price')
 });
+
+let allPriceSpan = null;
 
 function decreaseCount(index) {
     if (listOfFruitsCount[index] > 0) {
         listOfFruitsCount[index]--;
+        allPrice -= allPricelist[index];
     }
     else {
         listOfFruitsCount[index] = 0;
     }
-    const countSpan = document.querySelector(`span.count:nth-child(${index+1})`);
+    const countSpan = document.querySelectorAll(`.count`)[index];
     countSpan.textContent = listOfFruitsCount[index];
+    allPriceSpan.value = allPrice;
 }
 
 function increaseCount(index) {
     listOfFruitsCount[index]++;
-    const countSpan = document.querySelector(`span.count:nth-child(${index+1})`);
+    allPrice += allPricelist[index];
+    const countSpan = document.querySelectorAll(`.count`)[index];
     countSpan.textContent = listOfFruitsCount[index];
+    allPriceSpan.value = allPrice;
 }
 
 function clearAll() {
-    listOfFruitsCount = [];
-    const countSpans = document.querySelectorAll('span.count');
+    listOfFruitsCount.map(() => 0);
+    const countSpans = document.querySelectorAll('.count');
     countSpans.forEach(countSpan => {
         countSpan.textContent = 0;
     });
+    allPrice = 0;
+    allPriceSpan.value = allPrice;
 }
-
-/*<div class="container">
-    <div class="image-grid">
-    </div>
-    <div class="info-box">
-      这里是信息显示窗口
-    </div>
-  </div>
-*/
